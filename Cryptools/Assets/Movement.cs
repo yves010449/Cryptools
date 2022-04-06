90c351fe27e9dc8cb348db8e7f2a54bebe786dba
Yves <yves010449@gmail.com>
2022-04-06 15:11:24 +0800

    0.0.1.0

--- Cryptools/Assets/Movement.cs
+++ Cryptools/Assets/Movement.cs
@@ -16,6 +16,6 @@ public class Movement : MonoBehaviour
         this.movement = movementInput;
     }
     private void FixedUpdate() {
-        rb.velocity =  speed * movement;
+        rb.velocity =  speed * movement * Time.fixedDeltaTime;
     }
 }

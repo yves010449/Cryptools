28a9dd11887e12ed6ad0c9aae5b104dd1169dab5
Yves <yves010449@gmail.com>
2022-04-06 16:45:53 +0800

    0.0.2
    -movement
    -animation

--- Cryptools/Assets/Controller.cs
+++ Cryptools/Assets/Controller.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public Movement movement;
-
+    public AnimationController AnimController;
     private void Awake() {
         if(movement == null) {
             GetComponent<Movement>();
         }
+        if (AnimController == null) {
+            GetComponent<AnimationController>();
+        }
     }
     public void HandleMovement(Vector2 movement) {
         this.movement.Move(movement);
+    }
+    public void HandleAnimation(Vector2 movement) {
+        AnimController.Move(movement);
     }
 }

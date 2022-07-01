9b622d2ad6dbe5f40bdb04f28a26433cbf693d36
AkoSiHectorGagi <103095849+AkoSiHectorGagi@users.noreply.github.com>
2022-07-01 20:39:03 +0800

    0.4.9

--- Cryptools/Assets/Res/Creatures/Player/scripts/PlayerMovement.cs
+++ Cryptools/Assets/Res/Creatures/Player/scripts/PlayerMovement.cs
@@ -21,64 +21,84 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        try
         {
-
-            hit = Physics2D.Raycast(transform.position + (transform.right * rayCastX) + (transform.up * rayCastY), rayCastDirection, .5f);
-
-            if (hit)
+            if (Input.GetMouseButtonDown(0))
             {
-                anim.SetBool("IsMining", true);
-                Debug.Log("Hit!");
+
+                hit = Physics2D.Raycast(transform.position + (transform.right * rayCastX) + (transform.up * rayCastY), rayCastDirection, .5f);
+                if (hit)
+                {
+                    anim.SetBool("IsMining", true);
+                    Debug.Log("Hit!");
+                }
+            }
+            Debug.DrawRay(transform.position + (transform.right * rayCastX) + (transform.up * rayCastY), rayCastDirection * .5f, Color.red);
+            if (Input.GetMouseButtonUp(0))
+            {
+                anim.SetBool("IsMining", false);
+            }
+
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+            anim.SetFloat("Speed", movement.sqrMagnitude);
+
+            if (movement.y == 1 && movement.x == 0)
+            {
+                anim.SetFloat("LastMoveY", 1);
+                anim.SetFloat("LastMoveX", 0);
+                rayCastDirection.x = 0;
+                rayCastDirection.y = 1;
+            }
+            else if (movement.y == 0 && movement.x == 1)
+            {
+                anim.SetFloat("LastMoveY", 0);
+                anim.SetFloat("LastMoveX", 1);
+                rayCastDirection.x = 1;
+                rayCastDirection.y = 0;
+            }
+            else if (movement.y == -1 && movement.x == 0)
+            {
+                anim.SetFloat("LastMoveY", -1);
+                anim.SetFloat("LastMoveX", 0);
+                rayCastDirection.x = 0;
+                rayCastDirection.y = -1;
+            }
+            else if (movement.y == 0 && movement.x == -1)
+            {
+                anim.SetFloat("LastMoveY", 0);
+                anim.SetFloat("LastMoveX", -1);
+                rayCastDirection.x = -1;
+                rayCastDirection.y = 0;
             }
         }
-        Debug.DrawRay(transform.position + (transform.right * rayCastX) + (transform.up * rayCastY), rayCastDirection * .5f, Color.red);
-        if (Input.GetMouseButtonUp(0))
+        catch (System.Exception)
         {
-            anim.SetBool("IsMining", false);
-        }
 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.sqrMagnitude);
-
-        if (movement.y == 1 && movement.x == 0)
-        {
-            anim.SetFloat("LastMoveY", 1);
-            anim.SetFloat("LastMoveX", 0);
-            rayCastDirection.x = 0;
-            rayCastDirection.y = 1;
-        }
-        else if (movement.y == 0 && movement.x == 1)
-        {
-            anim.SetFloat("LastMoveY", 0);
-            anim.SetFloat("LastMoveX", 1);
-            rayCastDirection.x = 1;
-            rayCastDirection.y = 0;
-        }
-        else if (movement.y == -1 && movement.x == 0)
-        {
-            anim.SetFloat("LastMoveY", -1);
-            anim.SetFloat("LastMoveX", 0);
-            rayCastDirection.x = 0;
-            rayCastDirection.y = -1;
-        }
-        else if (movement.y == 0 && movement.x == -1)
-        {
-            anim.SetFloat("LastMoveY", 0);
-            anim.SetFloat("LastMoveX", -1);
-            rayCastDirection.x = -1;
-            rayCastDirection.y = 0;
         }
 
     }
     public void collect()
     {
-        stone.anim.SetFloat("HitPoints", stone.anim.GetFloat("Hitpoints") - 1);
+        try
+        {
+            hit.transform.GetComponent<Animator>().SetFloat("HitPoint", hit.transform.GetComponent<Animator>().GetFloat("HitPoint") - 1f);
+        }
+        catch (System.Exception)
+        {
+            anim.SetBool("IsMining", false);
+        }
     }
+
+    public void checkIfCollected()
+    {
+        if (hit.transform.GetComponent<Animator>().GetFloat("HitPoint") == 0)
+            anim.SetBool("IsMining", false);
+    }
+
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
--- Cryptools/Assets/Res/Objects/Stone/scripts/StoneBehavior.cs
+++ Cryptools/Assets/Res/Objects/Stone/scripts/StoneBehavior.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StoneBehavior : MonoBehaviour
-{
-    public Animator anim;
-
-    private void Update()
+{ 
+    public void breakStone()
     {
-
-    }
-    public void collect()
-    {
-        anim.SetFloat("HitPoints", anim.GetFloat("Hitpoints") - 1);
+        Destroy(gameObject);
     }
 }
